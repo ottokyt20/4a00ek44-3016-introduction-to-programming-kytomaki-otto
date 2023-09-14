2875f9f0a09da2938ba3c7fd4ef01af048bd1a1c
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-09-14 10:37:46 +0300

    added

---
+++ homework/03/e07/Main.java
@@ -0,0 +1,24 @@
+
+import java.io.Console;  
+
+public class Main  {
+    public static void main(String [] args) {
+        Console c = System.console();
+        String nimet = "";
+        String nimi = "";
+
+
+        while(!nimi.equals("lopeta")) {       // !"jaska".equals("lopeta") = !false
+            System.out.println("Anna nimi");
+            nimi = c.readLine();
+
+            
+            nimet = nimet + nimi;  // nimet = "jaskajussikalle" + "lopeta"
+            if(nimi.equals("lopeta") == false){
+                System.out.println(nimet);
+            }else {
+                break;
+            }   
+        }
+    }
+}
