33c277d42f293c170cbdcc3b9335cf0d2e711b1f
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-09-15 16:34:10 +0300

    added e12 and 13

---
+++ homework/03/e12/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String [] args) {
+
+    for(int a =0; a<5; a++){
+        double randomi = Math.random();    // Math.random for-loopin sisällä, niin tulee 5 kertaa eri luku 
+        System.out.println(randomi);   
+        }
+    }
+}
---
+++ homework/03/e13/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String [] args) {
+
+    for(int a =0; a<5; a++){
+        double randomi = Math.random()* 5;   
+        int b = (int) randomi;
+    System.out.println(b);   
+        }
+    }
+}
