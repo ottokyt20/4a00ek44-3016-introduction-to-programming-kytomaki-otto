4f62e8087e16faabb2475c09030b4ad3f761c9dd
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-09-21 13:28:26 +0300

    added

---
+++ homework/04/e01/Main.java
@@ -0,0 +1,14 @@
+
+class Main {
+    public static void main(String [] args) {
+       int [] taulukko = new int[3];  
+ 
+       taulukko[0] = 12;
+       taulukko[1] = 99;
+       taulukko[2] = 32;
+       
+
+       System.out.println(taulukko[0]);
+       System.out.println(taulukko[2]);
+    }
+}
