d2ac4ba8cdd41e17a90820097909ac55464698b2
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-09-21 13:33:57 +0300

    added

---
+++ homework/04/e10/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String [] args) {
+        
+        double randomi = Math.random()* 10;   
+        int i = (int) randomi;
+        System.out.println(i);   
+        }
+    }
