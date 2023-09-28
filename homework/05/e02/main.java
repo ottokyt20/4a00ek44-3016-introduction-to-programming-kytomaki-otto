01a19f206d337effefd6b1d07b03cbf940a5a150
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-09-28 11:23:19 +0300

    added

---
+++ homework/05/e02/main.java
@@ -0,0 +1,15 @@
+
+class main {
+    public static void main(String [] args) {
+        tulostaTervehdys(); 
+        tulostaNimi();
+    }
+    
+    public static void tulostaTervehdys() {
+        System.out.print("Hei ");
+    }
+
+    public static void tulostaNimi() {
+        System.out.print("Otto");
+    }
+}
