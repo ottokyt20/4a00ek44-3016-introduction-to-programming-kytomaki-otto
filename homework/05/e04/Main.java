c3d35adb5e2a3f27efd1e602c6725bc06a9002f7
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-09-29 16:50:59 +0300

    added e04

---
+++ homework/05/e04/Main.java
@@ -0,0 +1,10 @@
+
+class Main {
+    public static void main(String [] args) {
+       String text = palautaPaivamaara();  // ajatellaan että merkkijonoon text tallennetaan päivämäärä (metodista)
+       System.out.println(text);  // tulostetaan merkkijono
+    }
+    public static String palautaPaivamaara() {  
+        return "29.9.2023";   //metodi palauttaa tuon päivämäärän :D
+    }
+}
