005e147ca42122e77f3d2be2ca7cf5d392652584
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-10-08 16:37:31 +0300

    added e03

---
+++ homework/06/e03/Main.java
@@ -0,0 +1,11 @@
+
+
+class Main {
+    public static void main(String [] args) {
+        tulostaPaivamaara();  //kutsutaan metodi
+    }
+
+    public static void tulostaPaivamaara() {
+        System.out.println("2023-10-29");   //metodi tulostaa tuon
+    }
+}   
