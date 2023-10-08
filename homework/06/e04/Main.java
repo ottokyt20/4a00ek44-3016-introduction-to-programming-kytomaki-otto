4d6bf81ef000d9b28fd80cc7645c33d1f801cf24
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-10-08 16:37:59 +0300

    added e04

---
+++ homework/06/e04/Main.java
@@ -0,0 +1,10 @@
+
+class Main {
+    public static void main(String [] args) {
+        System.out.println( palautaPaivamaara() );
+    }
+
+    public static String palautaPaivamaara() {
+        return "2023-10-29";
+    }
+}
