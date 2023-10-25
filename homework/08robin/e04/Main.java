894d9ec3fd0ee0e994111bc3c277a7237febb6eb
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-10-25 12:27:45 +0300

    4

---
+++ homework/08robin/e04/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        for(int i = 0; i < 15; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+        for (int i = 15; 0 < i; i--) {
+            System.out.println(i);
+        }
+    }
+}
