aafb5c254a42421280c38b709cf54430c7b6a6f4
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-10-25 12:27:17 +0300

    3

---
+++ homework/08robin/e03/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 2;   
+        
+        while ( i <= 12) {
+            System.out.println(i);
+            i++;
+        }
+   
+        System.out.println();
+
+        int j = 12;
+
+        while (j >=2){
+            System.out.println(j);
+            j--;
+        }
+   
+    }
+}
