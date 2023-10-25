4cf0461284e031e41384f24c9e29df0a699c683d
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-10-25 12:10:54 +0300

    added e2

---
+++ homework/07/e02/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String [] args) {
+        int [][] taulukko ={{1, 3, 9}, {2, 7, 3}};
+        
+        for (int i = 0; i < taulukko[0].length; i++) {
+            System.out.print(taulukko[0][i]);
+
+        }
+    }
+}
