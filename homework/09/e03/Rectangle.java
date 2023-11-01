d850ccb99f439c67b1d306ed7f904c63c8d0675c
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-11-01 20:09:05 +0200

    added

---
+++ homework/09/e03/Rectangle.java
@@ -0,0 +1,12 @@
+
+public class Rectangle {
+  public int height = 2;
+  public int width = 3;
+
+
+    public void printMe() {
+        System.out.println(width);
+        System.out.println(height);
+    }
+}
+
---
+++ homework/09/e03/TestRectangle.java
@@ -0,0 +1,8 @@
+public class TestRectangle {
+   public static void main(String [] args) {
+        
+        Rectangle a = new Rectangle();
+
+        a.printMe(); 
+   }
+}
