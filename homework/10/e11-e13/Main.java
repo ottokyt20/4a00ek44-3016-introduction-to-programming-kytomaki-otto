6a90c04d3130318617e45565ad16b302f5116c3e
Otto Kytömäki <otto.kytomaki@tuni.fi>
2023-11-08 21:14:04 +0200

    11,12,13"

--- homework/10/e11-e13/Main.java
+++ homework/10/e11-e13/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Koira a = new Koira();
-        a.synnyta(); // Kutsutaan Koira-luokan synnyta()-metodia (periytyy Nisakas-luokasta.) //e12
+        a.synnyta(); // Kutsutaan Koira-luokan synnyta()-metodia (periytyy Nisakas-luokasta.) // e12
         a.nuuhkiPyllyä();
         a.ääntelehdi();
 
@@ -9,36 +9,33 @@ public class Main {
         b.luoTaidetta();
         b.synnyta();
         b.ääntelehdi();
-    
     }
 }
 
 abstract class Nisakas {
     public void synnyta() {
-        System.out.println("Synnytän lapsen.");  //ei käänny koska perustana muille luokille, pitää tehdä periytyviä luokkia bro
+        System.out.println("Synnytän lapsen."); // ei käänny koska perustana muille luokille, pitää tehdä periytyviä luokkia bro
     }
-    abstract void ääntelehdi();  //e13
+
+    abstract void ääntelehdi(); // e13
 }
 
-class Koira extends Nisakas {  //e11 ja e12
+class Koira extends Nisakas { // e11 ja e12
     public void nuuhkiPyllyä() {
         System.out.println("Nuuhkii toisen koiran pyllyä");
     }
-  
+
     void ääntelehdi() {
         System.out.println("Hau hau");
-    }    
+    }
 }
-
 
 class Ihminen extends Nisakas {
     public void luoTaidetta() {
-        System.out.println("Luo taidetta");  //e13
+        System.out.println("Luo taidetta"); // e13
     }
-  
+
     void ääntelehdi() {
         System.out.println("Morooo");
     }
 }
-
-
